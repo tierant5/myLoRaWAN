e196f006c25a1dd1cf924b8342d6c6a23e134e91
Caleb Stewart <12718298+tierant5@users.noreply.github.com>
2021-08-11 15:58:37 -0500

    Add rx1, rx2 timeout modes

--- src/Device.py
+++ src/Device.py
@@ -1,5 +1,5 @@
 # from SX127x import LoRa
-from constants import DEVCLASS
+from constants import DEVCLASS, MODE
 from LoRaMAC import LoRaMAC
 from Radio import Radio, SX127X
 
@@ -12,9 +12,34 @@ class Device():
         self.__device_class = None
         self.__mac = None
         self.__radio = None
+        self.__rx1_timeout_mode = None
+        self.__rx2_timeout_mode = None
 
+        self.rx1_timeout = False
+        self.rx2_timeout = False
         self.mac = mac
         self.radio = radio
+
+    def on_rx_done(self):
+        self.rx1_timeout = False
+        self.rx2_timeout = False
+
+    def on_rx_timeout(self):
+        if not self.rx1_timeout:
+            self.rx1_timeout = True
+            self.rx2_timeout = False
+        elif not self.rx2_timeout:
+            self.rx1_timeout = True
+            self.rx2_timeout = True
+
+    def on_tx_done(self):
+        pass
+
+    def tx(self):
+        pass
+
+    def rx(self):
+        pass
 
     @property
     def device_class(self) -> DEVCLASS:
@@ -52,6 +77,34 @@ class Device():
         else:
             raise TypeError
 
+    @property
+    def rx1_timeout_mode(self) -> MODE:
+        return self.__rx1_timeout_mode
+
+    @rx1_timeout_mode.setter
+    def rx1_timeout_mode(self, rx1_timeout_mode):
+        if isinstance(rx1_timeout_mode, MODE):
+            if rx1_timeout_mode in [MODE.RXCONT, MODE.STDBY]:
+                self.__rx1_timeout_mode = rx1_timeout_mode
+            else:
+                raise ValueError(f'{rx1_timeout_mode} is not a supported mode')
+        else:
+            raise TypeError
+
+    @property
+    def rx2_timeout_mode(self) -> MODE:
+        return self.__rx2_timeout_mode
+
+    @rx2_timeout_mode.setter
+    def rx2_timeout_mode(self, rx2_timeout_mode):
+        if isinstance(rx2_timeout_mode, MODE):
+            if rx2_timeout_mode in [MODE.RXCONT, MODE.SLEEP]:
+                self.__rx2_timeout_mode = rx2_timeout_mode
+            else:
+                raise ValueError(f'{rx2_timeout_mode} is not a supported mode')
+        else:
+            raise TypeError
+
 
 class ClassA(Device):
     """ Class to define LoRaWAN Class A Device """
@@ -59,6 +112,8 @@ class ClassA(Device):
     def __init__(self, *args):
         super(ClassA, self).__init__(*args)
         self.device_class = DEVCLASS.CLASS_A
+        self.rx1_timeout_mode = MODE.STDBY
+        self.rx2_timeout_mode = MODE.SLEEP
 
 
 if __name__ == '__main__':
