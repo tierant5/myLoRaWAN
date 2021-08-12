f76ddf4b76c3beeffbe9687d648f60d09c1da958
Caleb Stewart <12718298+tierant5@users.noreply.github.com>
2021-08-12 18:10:08 -0500

    Add parameter inits

--- src/LoRaWAN.py
+++ src/LoRaWAN.py
@@ -6,6 +6,13 @@ class LoRaWAN(Device.ClassC):
 
     def __init__(self, *args):
         super(LoRaWAN, self).__init__(*args)
+        self.__dev_addr = None
+        self.__nwkskey = None
+        self.__appskey = None
+        self.__devnonce = None
+        self.__appeui = None
+        self.__frame_cnt = None
+        self.__join_type = None
 
     def on_rx_done(self):
         super(LoRaWAN, self).on_rx_done()
