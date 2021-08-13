c3f77760922c8ca2a70d5500841e8b5f66ae2055
Caleb Stewart <12718298+tierant5@users.noreply.github.com>
2021-08-13 18:05:22 -0500

    Add Features

--- src/LoRaPacket.py
+++ src/LoRaPacket.py
@@ -1,3 +1,4 @@
+from constants import FTYPE, CID, MAJOR
 
 
 class Field:
@@ -16,6 +17,10 @@ class BitField(Field):
         super(Field, self).__init__()
 
 
+class RFU:
+    """ Define a Reserved for Future Use Field. """
+
+
 class Frame:
     """ Define a generic frame. """
 
@@ -24,112 +29,240 @@ class Header:
     """ Define a generic header. """
 
 
-class LoRaPacket:
-    """ Define a LoRa Packet. """
+class MACCommand:
+    """ Define a MAC Command. """
 
     def __init__(self):
-        pass
+        self.__cid = None
+        self.__data = None
 
 
-class PHYPayload:
-    """ Define a LoRaWAN Physical Payload. """
-
-    def __init__(self):
-        self._mhdr = None
-        self._macpayload = None
-        self._join_request = None
-        self._join_accept = None
-        self._mic = None
+class FOpts:
+    """ Define a Frame Options Class. """
 
 
-class MHDR:
-    """ Define a MAC Frame Header. """
+class FCtrl:
+    """ Define a base FCtrl Class"""
 
     def __init__(self):
-        self._ftype = None
-        self._rfu = None
-        self._major = None
+        self.__adr = None
+        self.__ack = None
+        self.__foptslen = None
 
 
-class FType:
-    """ Define a MHDR Frame Type. """
-
-    def __init__(self):
-        self._type = None
-
-
-class RFU:
-    """ Define a Reserved for Future Use Field. """
-
-
-class Major:
-    """ Define a bit field for LoRaWAN version. """
+class FCtrl_Downlink(FCtrl):
+    """ Define a FHDR Frame Control for Downlinks. """
 
     def __init__(self):
-        self._version = None
+        super(FCtrl_Downlink, self).__init__()
+        self.__fpending = None
 
 
-class MACPayload:
-    """ Define a LoRaWAN MACPayload Frame. """
+class FCtrl_Uplink(FCtrl):
+    """ Define a FHDR Frame Control for Uplinks. """
 
     def __init__(self):
-        self._fhdr = None
-        self._fport = None
-        self._frmpayload = None
+        super(FCtrl_Uplink, self).__init__()
+        self.__adrackreq = None
+        self.__classb = None
 
 
 class FHDR:
     """ Define a MACPayload Frame Header. """
 
     def __init__(self):
-        self._devaddr = None
-        self._fctrl = None
-        self._fcnt = None
-        self._fopts = None
+        self.__ftype = None
+        self.__devaddr = None
+        self.__fctrl = None
+        self.__fcnt = None
+        self.__fopts = None
+
+    @property
+    def ftype(self) -> FTYPE:
+        return self.__ftype
+
+    @ftype.setter
+    def ftype(self, ftype):
+        if isinstance(ftype, FTYPE):
+            self.__ftype = ftype
+        else:
+            raise TypeError
+
+    @property
+    def fctrl(self) -> FCtrl:
+        return self.__fctrl
+
+    @fctrl.setter
+    def fctrl(self, fctrl):
+        if isinstance(fctrl, int):
+            if self.ftype == FTYPE.UNCONFDATAUP:
+                self.__fctrl = FCtrl_Uplink(fctrl, ack=True)
+            elif self.ftype == FTYPE.CONFDATAUP:
+                self.__fctrl = FCtrl_Uplink(fctrl, ack=False)
+            if self.ftype == FTYPE.UNCONFDATADOWN:
+                self.__fctrl = FCtrl_Downlink(fctrl, ack=True)
+            elif self.ftype == FTYPE.CONFDATADOWN:
+                self.__fctrl = FCtrl_Downlink(fctrl, ack=False)
+            else:
+                raise ValueError
+        else:
+            raise TypeError
+
+    @property
+    def fopts(self) -> FOpts:
+        return self.__fopts
+
+    @fopts.setter
+    def fopts(self, fopts):
+        if isinstance(fopts, int):
+            self.__fopts = FOpts(fopts, self.ftype)
+        else:
+            raise TypeError
 
 
-class FCtrl_Downlink:
-    """ Define a FHDR Frame Control for Downlinks. """
+class MACPayload:
+    """ Define a LoRaWAN MACPayload Frame. """
 
     def __init__(self):
-        self._adr = None
-        self._rfu = None
-        self._ack = None
-        self._fpending = None
-        self._foptslen = None
+        self.__ftype = None
+        self.__fhdr = None
+        self.__fport = None
+        self.__frmpayload = None
+
+    @property
+    def ftype(self) -> FTYPE:
+        return self.__ftype
+
+    @ftype.setter
+    def ftype(self, ftype):
+        if isinstance(ftype, FTYPE):
+            self.__ftype = ftype
+        else:
+            raise TypeError
+
+    @property
+    def fhdr(self) -> FHDR:
+        return self.__fhdr
+
+    @fhdr.setter
+    def fhdr(self, fhdr):
+        if isinstance(fhdr, int):
+            self.__fhdr = FHDR(fhdr, self.ftype)
+        else:
+            raise TypeError
 
 
-class FCtrl_Uplink:
-    """ Define a FHDR Frame Control for Uplinks. """
-
-    def __init__(self):
-        self._adr = None
-        self._adrackreq = None
-        self._ack = None
-        self._classb = None
-        self._foptslen = None
-
-
-class MIC:
-    """ Define a field for Message Integrity Code. """
-
-
-class JoinRequest:
+class JoinRequest(MACPayload):
     """ Define a LoRaWAN Join-Request Payload. """
 
     def __init__(self):
-        self._joineui = None
-        self._deveui = None
-        self._devnonce = None
+        self.__joineui = None
+        self.__deveui = None
+        self.__devnonce = None
 
 
-class JoinAccept:
+class JoinAccept(MACPayload):
     """ Define a LoRaWAN Join-Accept Payload. """
 
     def __init__(self):
-        self._joinnonce = None
-        self._netid = None
-        self._devaddr = None
-        self._dlsettings = None
-        self._rxdelay = None
-        self._cflist = None
+        self.__joinnonce = None
+        self.__netid = None
+        self.__devaddr = None
+        self.__dlsettings = None
+        self.__rxdelay = None
+        self.__cflist = None
+
+
+class MHDR:
+    """ Define a MAC Frame Header. """
+
+    def __init__(self):
+        self.__ftype = None
+        self.__major = None
+
+    @property
+    def ftype(self) -> FTYPE:
+        return self.__ftype
+
+    @ftype.setter
+    def ftype(self, ftype):
+        if isinstance(ftype, FTYPE):
+            self.__ftype = ftype
+        else:
+            raise TypeError
+
+    @property
+    def major(self) -> MAJOR:
+        return self.__major
+
+    @major.setter
+    def major(self, major):
+        if isinstance(major, MAJOR):
+            self.__major = major
+        else:
+            raise TypeError
+
+
+class PHYPayload:
+    """ Define a LoRaWAN Physical Payload. """
+
+    def __init__(self):
+        self.__mhdr = None
+        self.__macpayload = None
+        self.__mic = None
+
+    @property
+    def mhdr(self) -> MHDR:
+        return self.__mhdr
+
+    @mhdr.setter
+    def mhdr(self, mhdr):
+        if isinstance(mhdr, int):
+            self.__mhdr = MHDR(mhdr)
+        else:
+            raise TypeError
+
+    @property
+    def macpayload(self) -> MACPayload:
+        return self.__macpayload
+
+    @macpayload.setter
+    def macpayload(self, macpayload):
+        if isinstance(macpayload, int):
+            if self.mhdr.ftype == FTYPE.JOINACCEPT:
+                self.__macpayload = JoinAccept(macpayload)
+            elif self.mhdr.ftype == FTYPE.JOINREQUEST:
+                self.__macpayload = JoinRequest(macpayload)
+            else:
+                self.__macpayload = MACPayload(macpayload, self.mhdr.ftype)
+        else:
+            raise TypeError
+
+    @property
+    def mic(self) -> int:
+        return self.__mic
+
+    @mic.setter
+    def mic(self, mic):
+        if isinstance(mic, int):
+            self.__mic = mic
+        else:
+            raise TypeError
+
+
+class LoRaPacket:
+    """ Define a LoRa Packet. """
+
+    def __init__(self, *args):
+        self.__phypayload = None
+
+    @property
+    def phypayload(self, phypayload) -> PHYPayload:
+        return self.__phypayload
+
+    @phypayload.setter
+    def phypayload(self, phypayload):
+        if isinstance(phypayload, int):
+            self.__phypayload = PHYPayload(phypayload)
+        else:
+            raise TypeError
